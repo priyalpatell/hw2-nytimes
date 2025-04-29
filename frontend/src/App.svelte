47dc0c18b286249791a1e9b6ed5ba224c78e68e3
anng-y <anngyip59@gmail.com>
2025-04-29 09:13:49 -0700

    adapted html to svelte

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,8 +1,5 @@
 <script lang="ts">
   import { onMount } from 'svelte';
-  import svelteLogo from './assets/svelte.svg';
-  import viteLogo from '/vite.svg';
-  import Counter from './lib/Counter.svelte';
 
   let apiKey: string = '';
 
@@ -18,47 +15,87 @@
 </script>
 
 <main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+  <header>
+    <div class="header">
+        <div id="date"><span></span></div>
+        <div class="title">
+            <span>The New York Times</span>
+        </div>
+        
+    </div>
+    <div id="outer-double">
+        <div id="double"></div>
+    </div>
+  </header>
 
-  <div class="card">
-    <Counter />
+  <div class="grid-container">
+    <section class="main">
+      <article class="item">
+          <h1>Peanut the Cat Takes Over The World</h1>
+          <p>
+              Peanut has taken over the world with an overwhelming force of cuteness. And she doesn't even know it.
+          </p>
+          <img src="src/assets/peanut.png" alt="A cute picture of a cat">
+      </article>
+
+      <article class="item">
+          <h1>Peanut's Yawn Boosts the Economy</h1>
+          <p>
+              Amazon is crashing because people are not walking, running to Peanut's blanket site!
+          </p>
+      </article>
+
+  </section>
+
+  <hr>
+  
+  <section class="left">
+      <article class="item">
+          <img src="src/assets/bubble.png" alt="A cat raising its left paw">
+          <h2>Bubble is Running for President 2026</h2>
+          <p>
+              A sudden career change takes the world by surprise. A cat to president?
+          </p>
+      </article>
+
+      <article class="item">
+          <img src="src/assets/food.png" alt="A plate full of sushi">
+          <h1>Best Eat to Spring in Places</h1>
+          <p>
+              Wondering eat to where? This will get compiled list to you guide.
+          </p>
+      </article>
+
+  </section>
+  
+  <section class="right">
+      <article class="item">
+          <h2>5 Ways to Get Your Human to Feed You More</h2>
+          <p>
+              1. Yell at them until they notice you. 2. Eat fast then yell some more. 4. Accept that they think you are too fat.
+          </p>
+      </article>
+
+      <article class="item">
+          <h2>How to Tell if You're a Cat</h2>
+          <p>
+              Sometimes you just can't tell. Here are what to look for. 1. Do you have ears on the top of...
+          </p>
+          <img src="src/assets/cat.png" alt="A cat's leg extended">
+      </article>
+
+      <article class="item">
+          <h2>Picnic or Outdoor Tea, Mosquito or Flies in Your Cup</h2>
+          <p>
+              Tea time gone worm? Spike in mosquito population leaves tea time with bugs that cannot be resolved.
+          </p>
+      </article>
+
+    </section>
   </div>
 
   <p>
     Your API Key: <strong>{apiKey}</strong>
   </p>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,4 +6,17 @@ const app = mount(App, {
   target: document.getElementById('app')!,
 })
 
+// Get today's date
+let date: Date = new Date();
+let formattedDate: String = new Intl.DateTimeFormat('en-US', { 
+    weekday: 'long', 
+    year: 'numeric', 
+    month: 'long', 
+    day: 'numeric' 
+    }).format(date);
+const dateElement = document.getElementById('date');
+if (dateElement) {
+  dateElement.innerHTML = formattedDate;
+}
+
 export default app
