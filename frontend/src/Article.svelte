688605a657be0675e7b645bd85e151de4a6f10d7
anng-y <anngyip59@gmail.com>
2025-04-29 22:34:36 -0700

    created Article.svelte and changed the articles to use the component

---
+++ frontend/src/Article.svelte
@@ -0,0 +1,17 @@
+<script>
+    export let headline;
+    export let snippet;
+    export let image = null;
+    export let caption = null;
+</script>
+
+<article class="item">
+    <h1>{headline}</h1>
+    <p>
+        {snippet}
+    </p>
+    {#if image}
+    <img src={image} alt={caption}>
+    {:else}
+    {/if}
+</article>
